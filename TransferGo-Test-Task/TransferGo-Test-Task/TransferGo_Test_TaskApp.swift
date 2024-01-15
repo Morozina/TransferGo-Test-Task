e367d44242de894a109e473f0b208213872999a1
Morozina <zaryadina2014@gmail.com>
2024-01-15 20:26:35 +0100

    Created project

---
+++ TransferGo-Test-Task/TransferGo-Test-Task/TransferGo_Test_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransferGo_Test_TaskApp.swift
+//  TransferGo-Test-Task
+//
+//  Created by Vladyslav Moroz on 15/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct TransferGo_Test_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
